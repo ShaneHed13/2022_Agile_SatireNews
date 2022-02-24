8b9baab14c520b3706407e8571427ed1f6d6bd51
ShaneHed13 <shane.hedrington@gmail.com>
2022-02-24 15:57:24 -0600

    Got object separated from index.js file and working

--- index.js
+++ index.js
@@ -1,45 +1,6 @@
 (function() {
   let storyList = document.querySelector(".story-list");
 
-    let storyObjects = [
-    {
-      id: 1,
-      headline: "Test Story Headline 1",
-      date: "2/20/2022",
-      imgLink: "images/articles/010922193908.jpg",
-      blurb: "Test Story Blurb",
-      author: "Shane Hedrington",
-      Article: "Test Article, test article"
-    },
-    {
-      id: 2,
-      headline: "Test Story Headline 2",
-      date: "2/20/2022",
-      imgLink: "images/articles/010922193908.jpg",
-      blurb: "Test Story Blurb",
-      author: "Shane Hedrington",
-      Article: "Test Article, test article"
-    },
-    {
-      id: 3,
-      headline: "Test Story Headline 3",
-      date: "2/20/2022",
-      imgLink: "images/articles/010922193908.jpg",
-      blurb: "Test Story Blurb",
-      author: "Shane Hedrington",
-      Article: "Test Article, test article"
-    },
-    {
-      id: 4,
-      headline: "Test Story Headline 4",
-      date: "2/20/2022",
-      imgLink: "images/articles/010922193908.jpg",
-      blurb: "Test Story Blurb",
-      author: "Shane Hedrington",
-      Article: "Test Article, test article"
-    },
-  ];
-
   let storyItems = "";
 
   for (story of storyObjects) {
