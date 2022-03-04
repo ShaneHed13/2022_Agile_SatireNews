a86093ad7ab50fca956549d58d38362cd449c33a
ShaneHed13 <shane.hedrington@gmail.com>
2022-03-03 20:17:34 -0600

    Added semi-functional search function

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
     let newsStories = document.querySelector(".story-list");
 
     let storyItems = "";
-
     for (story of storyObjects) {
         storyItems += "<li><a href=article.html#id=" + story.id + "><img id='image' src=" + story.imgLink + "><p id='date'>" + story.date + "</p><h3 id='headline'>" + story.headline + "</h3><p id='blurb'>" + story.blurb + "</p></a></li>"
       
