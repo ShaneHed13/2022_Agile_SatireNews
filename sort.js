652ace6e183e52757cf0625763903316934c01be
ShaneHed13 <shane.hedrington@gmail.com>
2022-03-06 11:39:11 -0600

    [delivers #181335563, delivers #181410096] Polished Search Function, added sort function, added more test stories

---
+++ sort.js
@@ -0,0 +1,26 @@
+function sort_story() {
+    
+    let newsStories = document.querySelector(".story-list");
+    
+    let storyItems = "";
+    
+    storyObjects.sort((a, b) => {
+        
+        let da = new Date(a.date),
+            db = new Date(b.date);
+        return da - db;
+        
+    });
+    
+    
+    for (story of storyObjects){
+            
+            storyItems += "<li><a href=article.html#id=" + story.id + "><img id='image' src=" + story.imgLink + "><p id='date'>" + story.date + "</p><h3 id='headline'>" + story.headline + "</h3><p id='blurb'>" + story.blurb + "</p></a></li>"
+        
+            newsStories.innerHTML = story.sort;
+            
+    };
+    
+    newsStories.innerHTML = storyItems;
+    
+}
--- storyObject.js
+++ storyObject.js
@@ -2,7 +2,7 @@
     {
       id: 1,
       headline: "Joe Biden uninvites Putin from his birthday party",
-      date: "2/20/2022",
+      date: "2/24/2022",
       imgLink: "images/articles/bidenPutin.jpg",
       blurb: "Critics warn that this response may be too severe",
       author: "Shane Hedrington",
@@ -19,29 +19,38 @@
     },
     {
       id: 3,
-      headline: "Test Story Headline 3",
-      date: "2/20/2022",
-      imgLink: "images/articles/010922193908.jpg",
-      blurb: "Test Story Blurb",
+      headline: "Elon Musk destroys Russia",
+      date: "2/15/2022",
+      imgLink: "images/articles/musk.jpg",
+      blurb: "Elon Musk, destroyed Russia's entire army with precision laser strikes form his starlink satelites.",
       author: "Shane Hedrington",
-      article: "Test Article, test article"
+      article: "Elon Musk has become the first 'human' to even defeat a world superpower.  Russia immediately surrendered to Musk"
     },
     {
       id: 4,
-      headline: "Test Story Headline 4",
-      date: "2/20/2022",
-      imgLink: "images/articles/010922193908.jpg",
-      blurb: "Test Story Blurb",
+      headline: "Coronavirus Cured 1 day before State of the Union Address",
+      date: "3/3/2022",
+      imgLink: "images/articles/stateoftheunion.jpg",
+      blurb: "In a completely coincidental move, coronavirus was cured just prior to the United States, State of the Union address",
       author: "Shane Hedrington",
-      article: "Test Article, test article"
+      article: "Article about State of the Union Address"
     },
     {
       id: 245,
-      headline: "Test Story Headline 254",
-      date: "2/20/2022",
-      imgLink: "images/articles/010922193908.jpg",
-      blurb: "Test Story Blurb",
+      headline: "Ukrainan President, officially a badass",
+      date: "2/28/2022",
+      imgLink: "images/articles/ukrainePresident.jpg",
+      blurb: "Ukrainian President, Vladymir Zelenskiy leads a rag tag group of mercenaries to take down the Kremlin",
       author: "Shane Hedrington",
-      article: "Test Article, test article"
+      article: "Zelenskiy Story"
+    },
+        {
+      id: 248,
+      headline: "Sean Penn recounts startline experience at Ukraine/Poland border",
+      date: "2/17/2022",
+      imgLink: "images/articles/seanPenn.jpg",
+      blurb: "Penn later found out he was taken to the United States/Mexico Border",
+      author: "Shane Hedrington",
+      article: "Sean Penn Article"
     },
   ];
