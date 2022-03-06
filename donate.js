ead28c33cf4a62857a5718e57b280183ae7afcae
MBlaser360 <73309005+MBlaser360@users.noreply.github.com>
2022-03-06 16:40:00 -0600

    [finishes #181472819]

--- donate.js
+++ donate.js
@@ -1,10 +1,1 @@
 
-//Radio Buttons
-let radioButtons = document.querySelectorAll('input[type=radio]');
-for(i = 0; i < radioButtons.length; i++) {
-  radioButtons[i].onclick = function(e) {
-    if(e.ctrlKey) {
-      this.checked = false;
-    }
-  }
-}
