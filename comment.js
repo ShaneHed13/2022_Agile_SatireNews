5fec6dc5334231ea863a62bac64a2afb50878712
Zach Hendrickson <zachhen21@gmail.com>
2022-03-10 00:56:55 -0600

    [Delivers #181352921] Added functionality for users to contact the author

--- comment.js
+++ comment.js
@@ -24,3 +24,38 @@ function SubmitComment() {
   nameInput.value = "";
 
 }
+
+//messaging author
+function sendMessage() {
+	
+	event.preventDefault();
+	
+	//getting references to inputs
+	var nameMsg = document.getElementById("nameMsg").value;
+	var emailMsg = document.getElementById("emailMsg").value;
+	var messageMsg = document.getElementById("messageMsg").value;
+	
+	//Email validation code similar to submission
+	var mailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+		
+	//Checks input to see if it matches format	
+	if(document.getElementById("emailMsg").value.match(mailCheck))
+	{	
+		if(nameMsg.value != "" && emailMsg.value != "" && messageMsg != ""){
+			document.getElementById("nameMsg").value = "";
+			document.getElementById("emailMsg").value = "";
+			document.getElementById("messageMsg").value = "";
+		
+			document.getElementById("messageSubmit").innerHTML = "Message Sent!"
+		} else {
+			alert("Please fill all fields to send message");
+		}
+		return true;
+	}
+	else
+	{
+		alert("Invalid email. Correct format: example@example.com");  
+		return false;
+	}
+	
+}
