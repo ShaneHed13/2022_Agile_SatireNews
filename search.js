2d42d2964307484b81612d8f68d805e4b17a01a6
ShaneHed13 <shane.hedrington@gmail.com>
2022-03-12 08:56:30 -0600

    first fix of search function, lower case now searches

--- search.js
+++ search.js
@@ -2,13 +2,13 @@ function search_story() {
     
     let newsStories = document.querySelector(".story-list");
     
-    let keyword = document.getElementById('searchbar').value
+    let keyword = document.getElementById('searchbar').value.toLowerCase();
     
     let storyItems = "";
     
     for (story of storyObjects){
         
-        if(story.headline.includes(keyword)) {
+        if(story.headline.toLowerCase().includes(keyword)) {
             
             storyItems += "<li><a href=article.html#id=" + story.id + "><img id='image' src=" + story.imgLink + "><p id='date'>" + story.date + "</p><h3 id='headline'>" + story.headline + "</h3><p id='blurb'>" + story.blurb + "</p></a></li>"
             
@@ -16,6 +16,6 @@ function search_story() {
         
     }
     
-    newsStories.innerHTML = keyword + storyItems;
+    newsStories.innerHTML = storyItems;
     
 }
